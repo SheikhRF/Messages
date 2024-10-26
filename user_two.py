079394b651525c40d394246495c4851fde51773d
thinggggggggg <thomasgotch0413@hotmail.com>
2024-10-26 15:14:23 +0100

    poo

--- user_two.py
+++ user_two.py
@@ -1,19 +1,16 @@
 from uagents import Agent, Context, Model
- 
- 
+
 class Message(Model):
     message: str
- 
- 
-SEED_PHRASE = "ILoveSendingMessages"
+
+SEED_PHRASE = "ILoveSendingMessagesToo"
  
 # Copy the address shown below
 
- 
 # Now go to https://agentverse.ai, register your agent in the Mailroom by providing the address you just copied.
 # Then, copy the agent's mailbox key and insert it here below inline
 AGENT_MAILBOX_KEY = "4f598b12-d856-4f59-ae72-b7409396960f"
- 
+
 # Now your agent is ready to join the agentverse!
 agent = Agent(
     name="user1",
@@ -21,12 +18,9 @@ agent = Agent(
     mailbox=f"{AGENT_MAILBOX_KEY}@https://agentverse.ai",
 )
 
-
- 
 @agent.on_message(model=Message, replies={Message})
 async def handle_message(ctx: Context, sender: str, msg: Message):
     ctx.logger.info(f"Received message from {sender}: {msg.message}")
- 
- 
+
 if __name__ == "__main__":
     agent.run()
