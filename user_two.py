6c318594150689a4323ad6ff99c252c6c09d6ded
SheikhRF <sheikh.rf@oundleschool.org.uk>
2024-10-26 15:20:36 +0100

    user 2 mods

--- user_two.py
+++ user_two.py
@@ -9,14 +9,15 @@ SEED_PHRASE = "ILoveSendingMessagesToo"
 
 # Now go to https://agentverse.ai, register your agent in the Mailroom by providing the address you just copied.
 # Then, copy the agent's mailbox key and insert it here below inline
-AGENT_MAILBOX_KEY = "4f598b12-d856-4f59-ae72-b7409396960f"
+AGENT_MAILBOX_KEY = "3fc55f41-1f70-4bbf-ad06-bf7bb5b2ee5f"
 
 # Now your agent is ready to join the agentverse!
 agent = Agent(
-    name="user1",
+    name="user2",
     seed=SEED_PHRASE,
     mailbox=f"{AGENT_MAILBOX_KEY}@https://agentverse.ai",
 )
+
 
 @agent.on_message(model=Message, replies={Message})
 async def handle_message(ctx: Context, sender: str, msg: Message):
