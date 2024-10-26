ff62ce779b3e34be639ca627d8240f94a91f3b5b
SheikhRF <sheikh.rf@oundleschool.org.uk>
2024-10-26 16:18:06 +0100

    kool koding klub

--- user_two.py
+++ user_two.py
@@ -3,25 +3,36 @@ from uagents import Agent, Context, Model
 class Message(Model):
     message: str
 
-SEED_PHRASE = "ILoveSendingMessagesToo"
- 
-# Copy the address shown below
+class Message(Model):
+    message: str
 
-# Now go to https://agentverse.ai, register your agent in the Mailroom by providing the address you just copied.
-# Then, copy the agent's mailbox key and insert it here below inline
+class Request(Model):
+    text: str
+
+class Response(Model):
+    text: str
+    agent_address: str
+
+DESTINATION = "agent1qd4mdj8rv6zkhf9zu2uhzct95egmzv60tx0q05kgw003983a40585sdttjp"
+
+SEED_PHRASE = "ILoveSendingMessagesToo"
+
 AGENT_MAILBOX_KEY = "3fc55f41-1f70-4bbf-ad06-bf7bb5b2ee5f"
 
-# Now your agent is ready to join the agentverse!
 agent = Agent(
     name="user2",
     seed=SEED_PHRASE,
     mailbox=f"{AGENT_MAILBOX_KEY}@https://agentverse.ai",
 )
 
-
 @agent.on_message(model=Message, replies={Message})
 async def handle_message(ctx: Context, sender: str, msg: Message):
     ctx.logger.info(f"Received message from {sender}: {msg.message}")
+
+@agent.on_rest_post("/rest/post", Request, Response)
+async def handle_post(ctx: Context, req: Request) -> Response:
+    ctx.logger.info("Received POST request")
+    await ctx.send(DESTINATION, Message(message=req.text))
 
 if __name__ == "__main__":
     agent.run()
